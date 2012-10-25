5efed35989899f7a2acc4ad6b7a2b3744e153d9c
Steve Stagg <stestagg@gmail.com>
2012-10-25 20:59:19 +0100

    add bitfield:  Cython based efficient integer array with set-like operations

---
+++ bitfield/tests/bitfield_test.py
@@ -0,0 +1,134 @@
+import bitfield
+import unittest
+
+class BitfieldTest(unittest.TestCase):
+
+    def test_repr_eval(self):
+        b = bitfield.Bitfield()
+        b.add(100)
+        c = eval(repr(b))
+        self.assertEqual(b, c)
+        for i in range(0, 1000, 13):
+            b.add(i)
+        c = eval(repr(b))
+        self.assertEqual(b, c)
+
+    def test_count(self):
+        b = bitfield.Bitfield()
+        self.assertEqual(b.count, 0)
+        b.add(0)
+        self.assertEqual(b.count, 1)
+        b.add(10000)
+        self.assertEqual(b.count, 2)
+        self.assertEqual(len(b), 2)
+
+    def test_mutating_while_iterating(self):
+        b = bitfield.Bitfield([[0, 1000]])
+        count = len(b)
+        for num in b:
+            self.assertIn(num, b)
+            b.remove(num)
+            count -= 1
+            self.assertEqual(len(b), count)
+            self.assertNotIn(num, b)
+        self.assertEqual(count, 0)
+
+    def test_membership(self):
+        b = bitfield.Bitfield()
+        b.add(0)
+        b.add(1)
+        b.add(2)
+        self.assertTrue(1 in b)
+        self.assertFalse(3 in b)
+        self.assertEqual(list(b), [0, 1, 2])
+
+    def test_filling_page(self):
+        p = bitfield.IdsPage()
+        self.assertEqual(p.count, 0)
+        chunk_bytes, page_chunks = bitfield.get_sizes()
+        bits_per_chunk = page_chunks * chunk_bytes * 8
+        for i in range(bits_per_chunk):
+            p.add(i)
+        with self.assertRaises(AssertionError):
+            p.add(i+1)
+
+    def test_add_remove(self):
+        b = bitfield.Bitfield()
+        self.assertEqual(list(b), [])
+        for i in xrange(0, 1000000, 881):            
+            b.add(i)
+            self.assertEqual(list(b), [i])
+            b.remove(i)
+
+    def test_merging(self):
+        a = bitfield.Bitfield()
+        b = bitfield.Bitfield()
+        a.add(0)
+        b.add(0)
+        a.update(b)
+        self.assertEqual(list(a), [0])
+        a.add(1000)
+        b.add(1000000)
+        b.update(a)
+        self.assertEqual(list(b), [0, 1000, 1000000])
+
+    def test_in(self):
+        a = bitfield.Bitfield()
+        for i in range(0, 100, 13):
+            a.add(i)
+        self.assertIn(13, a)
+        self.assertIn(0, a)
+        self.assertIn(26, a)
+        self.assertNotIn(27, a)
+        self.assertIn(39, a)
+        self.assertNotIn(1000000, a)
+        a.add(1000000)
+        self.assertIn(1000000, a)
+
+    def test_clone(self):
+        a = bitfield.Bitfield()
+        a.add(1)
+        a.add(10)
+        a.add(5000000)
+        b = a.clone()
+        self.assertEqual(a, b)
+        b.add(45)
+        self.assertNotEqual(a, b)
+
+    def test_symmetric_difference(self):
+        field = bitfield.Bitfield
+        a = field()
+        b = field()
+        a.add(1)
+        self.assertEqual(a.symmetric_difference(b), field([1]))
+        self.assertEqual(a ^ b, a)
+        b.add(2)
+        self.assertEqual(list(a ^ b), list(field([1,2])))
+
+        full = field([[1, 100000]]) 
+        full_set = set(range(1, 100000))
+        self.assertEqual(set(full), full_set)
+        self.assertEqual(set(full ^ a), full_set - set([1]))
+
+        odds = field(range(1, 90000, 2))
+        evens = field(range(0, 90000, 2))
+        full = odds ^ evens
+        self.assertEqual(list(full), range(90000))
+        self.assertEqual(len(full), len(odds) + len(evens))
+        self.assertEqual(len((odds ^ odds) | (evens ^ evens)), 0)
+
+    def test_creating_large_field(self):
+        # This is a strange test, the idea is to create a large set, then do something with it
+        # provided the test completes in a 'reasonable' timescale, then it should be fine
+        one_million = 1000000
+        size = one_million * 1000
+
+        field1 = bitfield.Bitfield([[0, size]])
+        field2 = bitfield.Bitfield([[size, size*2]])
+        self.assertEqual(len(field1), size)
+        self.assertEqual(len(field2), size)
+        self.assertEqual(len(field1 | field2), size * 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
